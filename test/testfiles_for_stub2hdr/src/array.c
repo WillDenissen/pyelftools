b310ae53425ca9614838d2af021aa73556ab2254
Will Denissen <will.denissen@asml.com>
2022-10-13 09:24:45 +0200

    cleanup

--- test/testfiles_for_stub2hdr/src/array.c
+++ test/testfiles_for_stub2hdr/src/array.c
@@ -1,4 +1,9 @@
-int *var[10][20];
+// common
+int v1[2];
+int *v2[10][20];
 
 int f(int *var[30]) {}
 int g(int **var) {}
+
+// WIP 
+// int *(*v3[10][20])[30];
--- test/testfiles_for_stub2hdr/src/bad_init_var.c
+++
@@ -1,5 +0,0 @@
-struct vect_t {
-    int x,y;
-};
-
-struct vect_t var = {.x = 3, .y = 4};
